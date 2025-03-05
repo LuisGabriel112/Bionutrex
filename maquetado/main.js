037444d55b25d1328b76e798b24b88febc370396
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2025-03-05 08:43:27 -0600

    Add files via upload

--- maquetado/main.js
+++ maquetado/main.js
@@ -1,1 +1,8 @@
-console.log("hola mundo");
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
